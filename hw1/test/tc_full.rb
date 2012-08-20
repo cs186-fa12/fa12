cee2722b06f562a3765f59407c69cd72f9dd0394
Anirudh Todi <atodi@twitter.com>
2012-08-20 16:05:35 -0700

    Deleted hw1

--- hw1/test/tc_full.rb
+++
@@ -1,9 +0,0 @@
-require 'test/unit'
-require './test/diff_files'
-REFERENCE_FILE_PATH = "/home/ff/cs186/sp12/hw1"
-
-class FullTest < Test::Unit::TestCase
-  def test_full
-    diff_files("enron_with_categories")
-  end
-end
