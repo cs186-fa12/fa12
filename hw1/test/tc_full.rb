b35742cad7acbe2ba21470b485913298b76ba95c
anirudhtodi <anirudhtodi@berkeley.edu>
2012-08-29 16:44:55 -0700

    Update hw1/test/tc_full.rb

--- hw1/test/tc_full.rb
+++ hw1/test/tc_full.rb
@@ -4,6 +4,6 @@ REFERENCE_FILE_PATH = "/home/ff/cs186/fa12/hw1"
 
 class FullTest < Test::Unit::TestCase
   def test_full
-    diff_files("enron_with_categories")
+    diff_files("enron_full")
   end
 end
