1ca972ab3f4975ef398c0bce7b41a06baa5f4fa5
anirudhtodi <anirudhtodi@berkeley.edu>
2012-08-29 16:03:00 -0700

    Update hw1/test/tc_sanity.rb

--- hw1/test/tc_sanity.rb
+++ hw1/test/tc_sanity.rb
@@ -1,6 +1,6 @@
 require 'test/unit'
 require './test/diff_files'
-REFERENCE_FILE_PATH = "/home/ff/cs186/sp12/hw1"
+REFERENCE_FILE_PATH = "/home/ff/cs186/fa12/hw1"
 
 class SanityTest < Test::Unit::TestCase
   def test_sanity
